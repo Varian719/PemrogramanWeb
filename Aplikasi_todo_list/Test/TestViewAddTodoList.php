d2573ac9e9478b578e94afb0ea35a59f214d7b90
Varian719 <72840944+Varian719@users.noreply.github.com>
2024-03-25 11:58:41 +0800

    Senin

---
+++ Aplikasi_todo_list/Test/TestViewAddTodoList.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "../View/ViewAddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Eko");
+addTodoList("Kurniawan");
+addTodoList("Khannedy");
+
+viewAddTodoList();
+
+showTodoList();
+
+viewAddTodoList();
+
+showTodoList();
+?>
